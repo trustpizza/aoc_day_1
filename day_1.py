6535bb42670db6ece01c4fac0e57ccddcc51e837
trustpizza <maxelolsson@gmail.com>
2023-03-30 14:28:45 -0500

    Finish part two

--- day_1.py
+++ day_1.py
@@ -17,16 +17,33 @@ def find_elven_lists_sums(list):
     return out
 
 def find_elven_calories(list):
-    seperate_lists_for_each_elf = find_elven_lists_sums(list.split('\n'))
+    # First, we need to clean the list
+    # For the list, first split at eat lines
+    #print(list.split("\n"))
+    seperate_lists_for_each_elf = find_elven_lists_sums(list)
+    # print(seperate_lists_for_each_elf)
     return seperate_lists_for_each_elf
 
+def clean_and_find_cals(dirty_list):
+    return elf_with_most_calories(clean_list(dirty_list))
+
+def clean_list(dirty_list):
+    return dirty_list.split('\n')
+
 def elf_with_most_calories(list):
+    print(list)
     return max(find_elven_calories(list))
 
-def top_3_elves(list):
-    list = find_elven_calories(list)
-    highest = elf_with_most_calories(list)
-    return list
+def top_3_elves(list, times):
+    out = []
+    list_of_cals = find_elven_calories(list.split('\n'))
+
+    for i in range(times):
+        highest = max(list_of_cals)
+        out.append(highest)
+        list_of_cals.remove(highest)
+
+    return sum(out)
 
 print(top_3_elves("""
 1000
@@ -43,11 +60,11 @@ print(top_3_elves("""
 9000
 
 10000
-"""))
-    
+""", 3))
+
 
 """
-print(elf_with_most_calories("""
+print(clean_and_find_cals("""
 1000
 2000
 3000
